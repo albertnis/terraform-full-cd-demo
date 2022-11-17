7d407866debb73c56bacc8ce69ac46ab960dc738
Albert Nisbet <albertnis@gmail.com>
2022-11-17 18:13:19 +1300

    Migrate from TF cloud to S3 backend

---
+++ src/worker.js
@@ -0,0 +1,15 @@
+const html = `<!DOCTYPE html>
+<body>
+  <h1>Hello from this deployment</h1>
+  <h2>Now with a swanky h2, woo</h2>
+  <p>The environment name is ${ENV_NAME}</p>
+</body>`;
+
+const handleRequest = (request) =>
+  new Response(html, {
+    headers: { "content-type": "text/html;charset=UTF-8" },
+  });
+
+addEventListener("fetch", (event) =>
+  event.respondWith(handleRequest(event.request))
+);
